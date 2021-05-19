6b6aee21b4d1d47744db082c06bf20205b824ca4
BasHolweg <b.holweg5@gmail.com>
2021-05-19 23:29:49 +0200

    new exersersizes

--- Practicum_1b_controle.py
+++ Practicum_1b_controle.py
@@ -2,14 +2,20 @@ import numpy as np
 import matplotlib.pylab as plt
 import control
 import control.matlab as cm
+import scipy.signal as sig
 
 teller = np.array([5.0])
 noemer = np.array([1.0, 5])
 
 H = control.tf(teller, noemer)
+H1 = sig.lti(teller, noemer)
 
 t = np.arange(0, 10, 0.01)
-u = np.sin(5*t)
-y, T, xout = cm.lsim(H, u, t)
+u = np.sin(5.*t)
 
+tout, y, x = sig.lsim(H1, u, t)
+# y, T, xout = cm.lsim(H, u, t)
+
+plt.plot(t, y)
+plt.plot(t, u)
 plt.show()
