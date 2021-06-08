1018fee7239b1999d60a82408dcf171b81115c2a
BasHolweg <b.holweg5@gmail.com>
2021-06-08 16:43:19 +0200

    finished Practicum_2.py

--- Practicum_2_PD.py
+++ Practicum_2_PD.py
@@ -4,24 +4,30 @@ import control
 
 m = 1
 Kp = 100
+Kd = 20
 
-teller = np.array([1.0])
-noemer = np.array([m, 0., 0.])
+L = control.tf([Kd, Kp], [1.])
+H = control.tf([1.0], [m, 0., 0.])
 
-H = control.tf(teller, noemer)
+print(L)
 print(H)
 
-Sys1 = Kp*H
+Sys1 = L*H
 Sys2 = 1
+Sys3 = Kp*H
+
+Hp = control.feedback(Sys3, Sys2)
 Hclosed = control.feedback(Sys1, Sys2)
+
 t, y = control.step_response(Hclosed)
 polen = control.pole(Hclosed)
 
 control.bode_plot(Sys1)
+control.bode_plot(Hp)
 
 print(polen)
 
-# plt.plot(t, y)1
+# plt.plot(t, y)
 # plt.ylabel('Stap responsie')
 # plt.xlabel('Tijd [s]')
 plt.show()
