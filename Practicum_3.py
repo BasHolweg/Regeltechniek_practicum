2560cb3d1e78010e9f44a6e727b99f4a1197037b
BasHolweg <b.holweg5@gmail.com>
2021-06-09 18:17:36 +0200

    finished Practicum_3.py

--- Practicum_3.py
+++ Practicum_3.py
@@ -3,32 +3,34 @@ import matplotlib.pylab as plt
 import control
 
 m = 1
-Kp = 20
-Ki = 1
-Kd = 5
+Kp = 1
+Ki = 0
+Kd = 0
 
 I = control.tf([Ki], [1.0, 0.])
 D = control.tf([Kd, 0.], [1.])
-G = control.tf([1.0], [m, -5., 0.])
+G = control.tf([1.0], [m, 5., 0.])
 
-print(I)
-print(D)
-print(G)
+# print(D)
+# print(I)
+print("G(s):",G)
 
-L = (Kp + I + D)*G
+L = (Kp + I + D) * G
 Sys2 = 1
 
-print(L)
+print("L(s):",L)
 
 Hclosed = control.feedback(L, Sys2)
 
+# t, y = control.step_response(G)
 t, y = control.step_response(Hclosed)
 
-print(Hclosed)
+print("Hgs(s):",Hclosed)
 
-# control.bode_plot(Hclosed)
+control.bode_plot(G)
 
-plt.plot(t, y)
-plt.ylabel('Stap responsie')
-plt.xlabel('Tijd [s]')
+# plt.plot(t, y)
+# plt.title(f"Kp:{Kp} Ki:{Ki}")
+# plt.ylabel('Stap responsie')
+# plt.xlabel('Tijd [s]')
 plt.show()
